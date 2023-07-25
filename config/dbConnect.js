d0043bf7dd82c2197ee9d5253179d63df70564b9
frederic zai <frederic.zai@gmail.com>
2023-07-25 17:45:01 +0200

    connect modif 17:44

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,15 +1,19 @@
 import mongoose from 'mongoose';
 
 const dbConnect = async () => {
-    try{
-        mongoose.set('strictQuery',false)
-        const connected = mongoose.connect(process.env.MONGO_URL)
-        console.log(`DATABASE CONNECTED ${(await connected).connection.host}`)
-    }catch(error){
+    try {
+        mongoose.set('strictQuery', false);
+        const connected = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`DATABASE CONNECTED ${connected.connection.host}`);
+    } catch (error) {
         console.log(error.message);
     }
-}
+};
 
 export default dbConnect;
+
 
 // vDI1ak3ExtRoyhf5
