51a616b7aa98700e9ce90954977ae5e13bbb46a7
frederic zai <frederic.zai@gmail.com>
2023-07-26 08:39:07 +0200

    remove origin cors

--- app/app.js
+++ app/app.js
@@ -18,7 +18,6 @@ import { globalErrHandler, notFound } from '../middleware/globaleErrHandler.js';
 
 const app = express(); // Invoke express as a function to create the app
 app.use(express.json());
-app.use(cors({ origin: 'https://basketball-demo.netlify.app' }));
 
 
 // DOTENV, TO USE ENV VARIABLE IN ALL APPLICATIONS
